08c0a78dd2a2351add8abe1ce1c3ef9c58d965a5
Isamu Press Nakagawa <i@IGNAKAGAWA-01.WIFI.WPI.EDU>
2015-09-07 17:36:17 -0400

    add arrows to main

--- assignment1/source/javaSource/src/Main.java
+++ assignment1/source/javaSource/src/Main.java
@@ -4,7 +4,16 @@
 public class Main {
     public static void main(String[] Args) {
 
-        System.out.println("test");
+        new Main().printGraphLayout();
 
+    }
+
+    public void printGraphLayout() {
+        System.out.println("Graph Layout");
+        System.out.println(" " + " x " + "\u2192");
+        System.out.println("y");
+        System.out.println("\u2193");
+        System.out.println("End Graph Layout");
+        System.out.println("");
     }
 }
