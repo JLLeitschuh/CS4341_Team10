cff6eab82d4dc077bef004f8ab6d5e31c8d1fad0
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2015-09-08 18:27:44 -0400

    Added some testings

--- assignment1/source/javaSource/test/NeighborTest.java
+++ assignment1/source/javaSource/test/NeighborTest.java
@@ -1,4 +1,23 @@
-
 public class NeighborTest {
+//Create a random 3x3 board
+    Point testPoint1 =(0,0,4);
+    Point testPoint2 =(0,1,9);
+    Point testPoint3 =(0,2,2);
+
+    Point testPoint4 =(1,0,5);
+    Point testPoint4 =(1,1,3);
+    Point testPoint4 =(1,2,5);
+
+    Point testPoint4 =(2,0,9);
+    Point testPoint4 =(2,1,8);
+    Point testPoint4 =(2,2,6);
+
+    List<Point> testList = new ArrayList<Point>();
+    testList.add(testPoint1);
+    testList.add(testPoint2);
+
+    SquareGrid testGrid = (3,3,)
+
+//    Neighbor testNeighbor = new Neighbor();
 
 }
