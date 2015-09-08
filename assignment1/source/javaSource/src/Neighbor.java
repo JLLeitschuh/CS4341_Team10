0ffac99cf9e06810808da81c1028310211535edc
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2015-09-08 17:23:50 -0400

    Fixes Memory Overflow

--- assignment1/source/javaSource/src/Neighbor.java
+++ assignment1/source/javaSource/src/Neighbor.java
@@ -64,4 +64,29 @@ public class Neighbor {
         }
         return newSearchNodes;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Neighbor neighbor = (Neighbor) o;
+
+        if (costSoFar != neighbor.costSoFar) return false;
+        if (cameFrom != null ? !cameFrom.equals(neighbor.cameFrom) : neighbor.cameFrom != null) return false;
+        if (!point.equals(neighbor.point)) return false;
+        if (direction != neighbor.direction) return false;
+        return gridInstance.equals(neighbor.gridInstance);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = cameFrom != null ? cameFrom.hashCode() : 0;
+        result = 31 * result + point.hashCode();
+        result = 31 * result + direction.hashCode();
+        result = 31 * result + costSoFar;
+        result = 31 * result + gridInstance.hashCode();
+        return result;
+    }
 }
