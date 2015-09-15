d5fe7f769e421bab1d5fe56ec5480fcf7cffcb7d
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2015-09-15 19:19:59 -0400

    Fully defines AbstractIndividual

--- assignment2/src/edu/wpi/cs4341/ga/AbstractIndividual.java
+++ assignment2/src/edu/wpi/cs4341/ga/AbstractIndividual.java
@@ -1,5 +1,53 @@
 package edu.wpi.cs4341.ga;
 
-public abstract class AbstractIndividual {
+import java.util.Collections;
+import java.util.List;
 
+/**
+ * Defines an abstract implementation of an individual.
+ * An individual is made up of a combination of genes from the initial gene pool.
+ */
+public abstract class AbstractIndividual {
+    /**
+     * List of the gene segments that define this individual
+     */
+    protected List<IGene> geneSegments;
+
+    /**
+     * Creates an abstract individual.
+     * @param geneSegments The gene segments that make up this gene.
+     */
+    AbstractIndividual(List<IGene> geneSegments){
+        this.geneSegments = geneSegments;
+    }
+
+    /**
+     * Allows external objects to look at the genes in the individual
+     * @return An unmodifiable list of geneSegments that make up this gene.
+     */
+    public List<IGene> getGeneSegments(){
+        return Collections.unmodifiableList(geneSegments);
+    }
+
+    /**
+     * Crosses two Abstract Individuals in order to create children individuals
+     * @param otherIndividual The other individual to merge with
+     * @return The children of this merging
+     */
+    public List<AbstractIndividual> crossOver(AbstractIndividual otherIndividual){
+        assert false;
+        return Collections.emptyList();
+    }
+
+    /**
+     * Mutates the Individual changing its internal gene structure
+     */
+    public void mutate(){
+    }
+
+    /**
+     * Gets the fitness of this individual.
+     * @return A number that allows the fitness of this individual to be compared to other individuals.
+     */
+    public abstract float getFitness();
 }
