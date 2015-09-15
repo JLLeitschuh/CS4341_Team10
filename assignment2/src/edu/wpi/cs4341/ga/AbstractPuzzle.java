eed1d1210c151477a695e0782aa195bcbbb44d73
Isamu Press Nakagawa <i@Air.local>
2015-09-15 18:57:30 -0400

    Add methods in Astract puzzle, added parse file.

--- assignment2/src/edu/wpi/cs4341/ga/AbstractPuzzle.java
+++ assignment2/src/edu/wpi/cs4341/ga/AbstractPuzzle.java
@@ -1,5 +1,13 @@
 package edu.wpi.cs4341.ga;
 
+import java.util.List;
+
 public abstract class AbstractPuzzle {
-    
+
+    AbstractPuzzle(List<IGene> IGene) {}
+
+    abstract List<IGene> genePool();
+    abstract List<IGene> getIndividuals();
+    abstract List<IGene> getGenes();
+
 }
