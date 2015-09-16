8078d00784c9f3b233fc36355dda398dd26d5f26
Isamu Press Nakagawa <isamu.press.nakagawa@gmail.com>
2015-09-15 20:26:10 -0400

    puzzle one now adds genes to geneList.

--- assignment2/src/edu/wpi/cs4341/ga/AbstractPuzzle.java
+++ assignment2/src/edu/wpi/cs4341/ga/AbstractPuzzle.java
@@ -3,22 +3,22 @@ package edu.wpi.cs4341.ga;
 import java.util.Collections;
 import java.util.List;
 
-public abstract class AbstractPuzzle {
+public abstract class AbstractPuzzle<T> {
 
     /**
      * The list of the current genes.
      * @return
      */
-    protected List<Gene> genePool;
+    protected List<Gene<T>> genePool;
 
-    protected AbstractPuzzle(List<Gene> genePool) {
+    protected AbstractPuzzle(List<Gene<T>> genePool) {
         this.genePool = genePool;
     }
     /**
      * Return the individual genes.
      * @return
      */
-    List<Gene> getGenes() {
+    List<Gene<T>> getGenes() {
         return Collections.unmodifiableList(this.genePool);
     }
 
--- assignment2/src/edu/wpi/cs4341/puzzle1/Puzzle1.java
+++ assignment2/src/edu/wpi/cs4341/puzzle1/Puzzle1.java
@@ -4,17 +4,17 @@ import edu.wpi.cs4341.ga.AbstractIndividual;
 import edu.wpi.cs4341.ga.AbstractPuzzle;
 import edu.wpi.cs4341.ga.Gene;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Puzzle1 extends AbstractPuzzle {
+public class Puzzle1 extends AbstractPuzzle<Integer> {
     private final Integer targetValue;
     private final List<AbstractIndividual> initialPopulation;
 
     public Puzzle1(List<String> input) {
         super(createGenePool(input));
         this.targetValue = new Integer(input.get(0));
-        assert false;
         initialPopulation = null;
     }
 
@@ -23,10 +23,13 @@ public class Puzzle1 extends AbstractPuzzle {
         return Collections.unmodifiableList(initialPopulation);
     }
 
-    private static List<Gene> createGenePool(List<String> input){
+    private static List<Gene<Integer>> createGenePool(List<String> input){
+        List<Gene<Integer>> geneList = new ArrayList<>();
         List<String> geneStringList = input.subList(1, input.size());
-        
-        return null;
+        for(String geneString : geneStringList) {
+            geneList.add(new Gene<Integer>(new Integer(geneString)));
+        }
+        return geneList;
 
     }
 }
