3d39209b61d269078a80ec2ba8ff6262b4d55ca2
Isamu Press Nakagawa <isamu.press.nakagawa@gmail.com>
2015-09-17 21:25:00 -0400

    added unit test

--- assignment2/test/edu/wpi/cs4341/puzzle1/Puzzle1Test.java
+++ assignment2/test/edu/wpi/cs4341/puzzle1/Puzzle1Test.java
@@ -1,5 +1,6 @@
 package edu.wpi.cs4341.puzzle1;
 
+import edu.wpi.cs4341.ga.AbstractIndividual;
 import edu.wpi.cs4341.ga.Gene;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,6 +24,14 @@ public class Puzzle1Test {
         assertEquals("The size of the gene list is incorrect.", 4, geneList.size());
         assertEquals("The fist element in the gene sequence was not correct.", new Integer(2), geneList.get(0).get());
         assertEquals("The last element in the gene sequence was not correct.", new Integer(7), geneList.get(3).get());
+
+
+    }
+
+    @Test
+    public void testGetIndiviuals() {
+        List<AbstractIndividual> abstractIndividuals = puzzle1.getIndividuals();
+        assertEquals("The size of the indicidual list does not match the fixed population size",abstractIndividuals.size(), puzzle1.getPopulationSize());
     }
 
 }
