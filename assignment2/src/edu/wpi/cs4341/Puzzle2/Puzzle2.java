4a37d139784e320851b6dafa915936cfe75f88d2
trietpham91 <trietpham@TMPHAM.WIFI.WPI.EDU>
2015-09-22 18:02:39 -0400

    Update on Puzzle 2

--- assignment2/src/edu/wpi/cs4341/Puzzle2/Puzzle2.java
+++ assignment2/src/edu/wpi/cs4341/Puzzle2/Puzzle2.java
@@ -9,37 +9,25 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Puzzle2 extends AbstractPuzzle<Integer> {
-    List<Float> bin1 = new LinkedList<>();
-    List<Float> bin2 = new LinkedList<>();
-    List<Float> bin3 = new LinkedList<>();
+    LinkedList<String> bin1 = new LinkedList<>();
+    LinkedList<String> bin2 = new LinkedList<>();
+    LinkedList<String> bin3 = new LinkedList<>();
     private final List<AbstractIndividual> initialPopulation;
     private final int POPULATIONSIZE = 20;
-
+//Store each ten genes in each three bin
     public Puzzle2(List<String> input) {
         super(createGenePool(input));
-        for(int i =0; i <= 6; i++) {
-            this.bin1 = Float.parseFloat(input.get(i));
-        }
-        this.bin2 = input.subList(7,14);
-        this.bin3 = input.subList(15,20);
+        this.bin1 = new LinkedList<>(input.subList(0, 9));
+        this.bin2 = new LinkedList<>(input.subList(10, 19));
+        this.bin3 = new LinkedList<>(input.subList(20, 30));
 
         this.initialPopulation = new ArrayList<>();
 
         // fill the initial population with individuals, with random gene sequences
-        for(int i = 0; i < POPULATIONSIZE; i++) {
+        for (int i = 0; i < POPULATIONSIZE; i++) {
 //            this.initialPopulation.add(new PackedIndividual(getRandomGeneSequ);
         }
-
-        }
-    public void bin1_multiply(){
     }
-    public void bin2_sum(){
-
-    }
-    public void bin3_ignore(){
-
-    }
-
 
 
     @Override
@@ -57,14 +45,49 @@ public class Puzzle2 extends AbstractPuzzle<Integer> {
         return this.POPULATIONSIZE;
     }
 
-    private static List<Gene<Integer>> createGenePool(List<String> input){
+    private static List<Gene<Integer>> createGenePool(List<String> input) {
         List<Gene<Integer>> geneList = new ArrayList<>();
         List<String> geneStringList = input.subList(1, input.size());
-        for(String geneString : geneStringList) {
-            geneList.add(new Gene<Integer>(new Integer(geneString)));
+        for (String geneString : geneStringList) {
+            geneList.add(new Gene<Integer>(new Integer(String.valueOf(geneString))));
         }
         return geneList;
+    }
+    //Multiply each gene in bin1 together
+    private static void getMultipliedBin1(List<String> bin1) {
+        List<Float> result_Multi = new ArrayList<>();
+
+        for (int i = 0; i < bin1.size(); i++) {
+            result_Multi.add(
+                    Float.parseFloat(bin1.get(i)) * Float.parseFloat(bin1.get(i+1)));
+        }
+    }
+    //Add each gene in bin1 together
+    private static void getSumBin2(List<String> bin2) {
+        List<Float> result_Sum = new ArrayList<>();
+
+        for (int i = 0; i < bin2.size(); i++) {
+            result_Sum.add(
+                    Float.parseFloat(bin2.get(i)) + Float.parseFloat(bin2.get(i+1)));
+
+        }
+    }
+
+    private static void getIgnoredBin3(List<String> bin3) {
+        List<String> result_Ignored = new ArrayList<>();
+
+        for (int i = 0; i < bin3.size(); i++) {
+            result_Ignored.add(bin3.get(i));
+            ;
+
+        }
     }
 
 
 }
+
+
+
+
+
+
