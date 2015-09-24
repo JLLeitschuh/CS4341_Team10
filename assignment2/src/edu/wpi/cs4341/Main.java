105aadb31ab4794e31cd7cad34780c9c63c9e3d5
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2015-09-24 19:11:53 -0400

    Finalizes Main Runtime Arguments
    Arguments are now all working and the main function runs for a specified ammount of time

--- assignment2/src/edu/wpi/cs4341/Main.java
+++ assignment2/src/edu/wpi/cs4341/Main.java
@@ -7,29 +7,29 @@ import edu.wpi.cs4341.ga.Population;
 import edu.wpi.cs4341.puzzle1.Puzzle1;
 import edu.wpi.cs4341.puzzle3.Puzzle3;
 
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 public class Main {
-    private static int puzzleNumber;
+    private static int puzzleNumber = Integer.MIN_VALUE;
     private static String fileName;
+    private static int secondRuntime = Integer.MIN_VALUE;
 
     public static void main(String[] args) {
 
-        // Start timer for GA.
-        long startTime = new Date().getTime();
-
         // Read arguments.
-        if ( parseCmdArgs(args) != 0) {
-            System.out.println("Arguments invalid. Terminating.");
+        int returnValue = parseCmdArgs(args);
+        if ( returnValue == -1) {
+            System.err.println("Arguments invalid. Terminating.");
+            printHelp();
+            System.exit(-1);
+        } else if (returnValue == 1) {
+            printHelp();
+            System.exit(0);
+        } else if (puzzleNumber == Integer.MIN_VALUE || fileName == null || secondRuntime == Integer.MIN_VALUE){
+            System.err.println("Missing param");
+            printHelp();
             System.exit(-1);
         }
-
-
-        // Hard coded for testing purposes
-//        puzzleNumber = 1;
-//        fileName = "puzzleOneTest3.txt";
 
 
         System.out.println("Using puzzle: " + puzzleNumber + "\nUsing filename: " + fileName);
@@ -41,15 +41,18 @@ public class Main {
         Population currentPopulation = new Population(abstractPuzzle.getIndividuals(), 0);
         Algorithm algorithm = new Algorithm(abstractPuzzle);
 
+        // Start timer for GA.
+        long expectedEndTime = System.currentTimeMillis() + secondRuntime * 1000;
+        long startTime = System.currentTimeMillis();
         // Run GA for x generation
-        for (int i = 0; i < 100000; i++) {
+        while (System.currentTimeMillis() < expectedEndTime) {
             currentPopulation = algorithm.evolvePopulation(currentPopulation);
             algorithm.storeIfBestIndividual(currentPopulation.getBestIndividual(), currentPopulation.getGenerationNumber());
         }
 
-        long endTime = new Date().getTime();
+        long endTime = System.currentTimeMillis();
         System.out.println("elapsed milliseconds: " + (endTime - startTime));
-
+        System.out.println("Populations generated: " + currentPopulation.getGenerationNumber());
         System.out.println("Best Gene's Fitness: " + algorithm.getBestIndividual().getFitness());
         System.out.println("Best Gene: " + algorithm.getBestIndividual());
         System.out.println("Best Gene from Population: " + algorithm.getBestIndividualPopulationNumber());
@@ -61,13 +64,23 @@ public class Main {
                 + (instance.totalMemory() - instance.freeMemory()) / mb + "MB");
     }
 
+    private static void printHelp(){
+        String helpText =
+                "The program accepts the following args\n" +
+                "\t--help              print this message\n" +
+                "\t--puzzle [1-3]      Use this puzzle\n" +
+                "\t--filename [file]   The file to read.\n" +
+                "\t--seconds [integer] The number of seconds to run the program";
+        System.out.println(helpText);
+    }
+
     static int parseCmdArgs(String[] args) {
         if (args.length == 0) {
             return -1;
         }
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("--help") || args.equals("/?")) {
-                return -1;
+                return 1;
             } else if (args[i].equals("--puzzle")) {
                 if (args[i + 1].equals("1")) {
                     puzzleNumber = 1;
@@ -76,13 +89,23 @@ public class Main {
                 } else if (args[i + 1].equals("3")) {
                     puzzleNumber = 3;
                 } else {
-                    System.out.println("Bad --puzzle value");
+                    System.err.println("Bad --puzzle value");
                     return -1;
                 }
                 i++;
             } else if (args[i].equals("--filename")) {
                 fileName = args[i + 1];
                 i++;
+            } else if (args[i].equals("--seconds")) {
+                try {
+                    secondRuntime = Integer.valueOf(args[i + 1]);
+                    i++;
+                } catch (NumberFormatException e){
+                    System.err.println("Bad --seconds value");
+                    System.err.println("Must be an integer value");
+                    e.printStackTrace();
+                    return -1;
+                }
             }
         }
         return 0;
